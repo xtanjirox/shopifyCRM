52e11184171fb254ce28513d0989e480fd3ab139
xtanjirox <xtanjirokamado93@gmail.com>
2023-10-26 23:28:21 +0200

    sign-in-up-out

--- shopify_crm/apps/core/views.py
+++ shopify_crm/apps/core/views.py
@@ -3,3 +3,11 @@ from django.views import generic
 
 class HomePage(generic.TemplateView):
     template_name = 'pages/index.html'
+
+
+class SignIn(generic.TemplateView):
+    template_name = 'pages/account/sign-in.html'
+
+
+class SignUp(generic.TemplateView):
+    template_name = 'pages/account/sign-up.html'
