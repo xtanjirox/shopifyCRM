47ea6dab57d73dd883a28799e8a03c63ddcf8f90
xtanjirox <xtanjirokamado93@gmail.com>
2023-10-30 07:48:41 +0100

    add models

--- shopify_crm/apps/core/admin.py
+++ shopify_crm/apps/core/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+from . import models
+
+
+admin.site.register(models.OptionProduct)
+admin.site.register(models.ProductImage)
+admin.site.register(models.Product)
+admin.site.register(models.ProductVariant)
+admin.site.register(models.PresentmentPrice)
